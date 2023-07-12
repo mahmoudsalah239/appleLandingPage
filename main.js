b198b6d0dd7072705464f190f4a5c9ee297f415a
mahmoud salah <46866945+mahmoudsalah239@users.noreply.github.com>
2023-07-12 23:49:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let posterimg = document.querySelector(".posterimg");
+function changmobile(src) {
+    posterimg.src = src;
+}
+
+function color(color){
+    document.body.style.backgroundColor=color
+}
